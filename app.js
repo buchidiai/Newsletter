94fe81e6829657018127b7fb547881409d87fc23
54 Bridge <54bridgeapp@gmail.com>
2020-01-04 10:43:24 -0600

    cleaned up html and added post route

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ const request = require("request");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const { firstname, lastname, email } = req.body;
+
+  const errorRes = value => `${value} is required`;
+
+  if (!firstname) {
+    return res.status(400).json(errorRes(firstname));
+  }
+  if (!lastname) {
+    return res.status(400).json(errorRes(lastname));
+  }
+  if (!email) {
+    return res.status(400).json(errorRes(email));
+  }
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`now listening on port ${PORT}`);
